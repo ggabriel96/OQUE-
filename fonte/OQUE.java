c697ae67260458445520bdec31c2e503b006657b
Cleiton Ambrosini <cleito.am@gmail.com>
2015-04-10 20:02:50 -0300

    mudado o nome da classe
    mudado o nome da classe

--- fonte/OQUE.java
+++ fonte/OQUE.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 import java.io.*;
 
-class Nome {
-    public static void main(String args[]) throws Exception 
+class OQUE {
+    public static void main(String args[]) throws Exception
     {
         File f;
         Scanner s;
         Interpretador b;
-        
+
         String linhas[] = new String[2000];
         f = new File(args[0]);
         s = new Scanner(f);
-        
+
         b = new Interpretador();
 
         int i = 0;
-        while(s.hasNext()) 
+        while(s.hasNext())
         {
             linhas[i] = s.nextLine();
             i++;
