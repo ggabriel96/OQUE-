4da4c03c3359e7ac50177f0e123343fcb70c666f
Cleiton Ambrosini <cleito.am@gmail.com>
2015-04-19 22:18:38 -0300

    Criado a lista double
    Lista double funcionando

---
+++ fonte/Double_node.java
@@ -0,0 +1,27 @@
+class Double_node{
+  private String nome = new String("");
+  private double valor;
+  private Double_node proximo;
+
+  public void setNome(String nome_var){
+    this.nome = nome_var;
+  }
+  public String getNome(){
+    return this.nome;
+  }
+
+  public void setValor(double valor_var){
+    this.valor = valor_var;
+  }
+  public double getValor(){
+    return this.valor;
+  }
+
+  public void setProximo(Double_node prox){
+    this.proximo = prox;
+  }
+  public Double_node getProximo(){
+    return this.proximo;
+  }
+
+}
