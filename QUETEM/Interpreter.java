333f949b0e86f13c785d6287c171499d6e802feb
vladimirbelinski <vladimir_belinski@hotmail.com>
2015-06-13 13:31:54 -0300

    Just added a header containing the description of the class.

--- QUETEM/Interpreter.java
+++ QUETEM/Interpreter.java
@@ -1,10 +1,20 @@
 package QUETEM;
+/*******************************************************************************
+Name: Interpreter.java
+Authors: Acácia dos Campos da Terra - terra.acacia@gmail.com
+         Gabriel Batista Galli - g7.galli96@gmail.com
+         Vladimir Belinski - vlbelinski@gmail.com
+
+Description: Class Interpreter of OQUE, a programming language based on Java.
+             Responsible for the execution of the code.
+*******************************************************************************/
+
 
 import java.util.*;
 import java.util.regex.*;
 
 class Interpreter {
-	
+
 	public void execute(ArrayList<Line> code) throws UatException {
 	}
 }
